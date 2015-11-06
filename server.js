5f1cb293e6b9968a11eb835fb4e9e67ca5af9cbf
Alicia Poindexter <alcampbel@gmail.com>
2015-11-05 22:58:30 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var http = require('http');
+var port = 3000;
+
+var server = http.createServer(function(req, res){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+
+
+});
+
+server.listen(port);
